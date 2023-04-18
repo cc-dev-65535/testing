a0527d3208e71029cd3bd47806130b4af782714f
Collin Chan <omiyage4949@gmail.com>
2023-04-17 21:37:58 -0700

    Add twitter and title tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,17 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+test('has correct title', async ({ page }) => {
+  await page.goto('https://www.tylertech.com');
+  await expect(page).toHaveTitle(/Tyler Technologies/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
+test('twitter link correct', async ({ page, context }) => {
+  await page.goto('https://www.tylertech.com');
+  const pagePromise = context.waitForEvent('page');
+  await page.getByTitle("Twitter").click();
+  const newPage = await pagePromise;
+  await newPage.waitForLoadState();
+  await expect(newPage).toHaveURL("https://twitter.com/tylertech");
 });
+
+
